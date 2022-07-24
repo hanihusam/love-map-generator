a115f1982ec0b25320c63b3ee5cab8210e8fd8d3
Hani Husamuddin <hani.husam@gmail.com>
2022-07-24 20:40:57 +0700

    chore: add missing depedencies and script

--- app/routes/cards/index.tsx
+++ app/routes/cards/index.tsx
@@ -1,8 +1,8 @@
 export default function CardsIndexRoute() {
   return (
     <div>
-      <p>Here's a random joke:</p>
+      <p>Here&quot;s a random joke:</p>
       <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
     </div>
-  )
+  );
 }
