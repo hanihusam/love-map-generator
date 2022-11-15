03b0d70c3baac477044224990bdeae4bfaf41329
Hani Husamuddin <hani.husam@gmail.com>
2022-11-15 16:04:55 +0700

    feat: implemented styles on each page

--- app/routes/cards.tsx
+++ app/routes/cards.tsx
@@ -1,12 +1,44 @@
-import {Outlet} from '@remix-run/react'
+import type { LinksFunction } from "@remix-run/node";
+import { Outlet, Link } from "@remix-run/react";
+
+import stylesUrl from "~/styles/cards.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
 
 export default function CardsRoute() {
   return (
-    <div>
-      <h1>J🤪KES</h1>
-      <main>
-        <Outlet />
+    <div className="cards-layout">
+      <header className="cards-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
+              <span className="logo">🤪</span>
+              <span className="logo-medium">J🤪KES</span>
+            </Link>
+          </h1>
+        </div>
+      </header>
+      <main className="cards-main">
+        <div className="container">
+          <div className="cards-list">
+            <Link to=".">Get a random joke</Link>
+            <p>Here are a few more jokes to check out:</p>
+            <ul>
+              <li>
+                <Link to="some-joke-id">Hippo</Link>
+              </li>
+            </ul>
+            <Link to="new" className="button">
+              Add your own
+            </Link>
+          </div>
+          <div className="cards-outlet">
+            <Outlet />
+          </div>
+        </div>
       </main>
     </div>
-  )
+  );
 }
