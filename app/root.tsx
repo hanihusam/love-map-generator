14ab5d478add4bf68cf783e5f0ab3f2da19467fb
Hani Husamuddin <hani.husam@gmail.com>
2022-11-15 16:00:49 +0700

    feat: added assets and css styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,17 @@
-import {LiveReload, Outlet} from '@remix-run/react'
+import { Links, LiveReload, Outlet } from "@remix-run/react";
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>Remix: So great, it's funny!</title>
+        <title>Remix: So great, it&apos;s funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet />
         <LiveReload />
       </body>
     </html>
-  )
+  );
 }
